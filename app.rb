98d6f96ebcb8ed73f821c1f6510d0ca9cebf7552
ocawthorne <ocawthorne@gmail.com>
2020-11-11 22:30:15 +0000

    sinatra-sessions-111120

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, 'flucloxacillin'
+    end
+
+    get('/') {erb :index}
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
 end
